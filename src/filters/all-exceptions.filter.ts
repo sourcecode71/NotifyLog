e15331276af7bd3cdefe1aeed77213af3eab956f
Mostafiz <mostafiz571@gmail.com>
2025-04-24 11:03:45 +0600

    added loader

---
+++ src/filters/all-exceptions.filter.ts
@@ -0,0 +1,34 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { LoggerService } from '../logger/services/logger.service';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly logger: LoggerService) {}
+
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status =
+      exception instanceof HttpException ? exception.getStatus() : 500;
+
+    const message =
+      exception instanceof Error ? exception.message : 'Internal server error';
+    const stack = exception instanceof Error ? exception.stack : undefined;
+
+    this.logger.error(`[${request.method}] ${request.url} - ${message}`, stack);
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message,
+    });
+  }
+}
