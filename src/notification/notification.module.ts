614bf047246f4c1fefce2badea8b7dd0974f1e7d
Mostafiz <mostafiz571@gmail.com>
2025-05-04 16:24:39 +0600

    feat: added the webhooks

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { NotificationController } from './presentation/controllers/notification.controller';
+import { WebhookController } from './presentation/controllers/webhook.controller';
 import { LogController } from './presentation/controllers/log.controller';
 import { NotificationRepository } from './infrastructure/repositories/notification.repository';
+import { WebhookRepository } from './infrastructure/repositories/webhook.repository';
 import { NotificationSchema } from './infrastructure/persistence/mongoose/notification.schema';
+import { WebhookSchema } from './infrastructure/persistence/mongoose/webhook.schema';
 import { NotificationFactory } from './application/factories/notification.factory';
 import { EmailNotificationStrategy } from './application/strategies/email-notification.strategy';
 import { SMSNotificationStrategy } from './application/strategies/sms-notification.strategy';
@@ -13,14 +16,19 @@ import { LoggerModule } from '../logger/logger.module';
   imports: [
     MongooseModule.forFeature([
       { name: 'Notification', schema: NotificationSchema },
+      { name: 'Webhook', schema: WebhookSchema }, // Register Webhook schema
     ]),
     LoggerModule,
   ],
-  controllers: [NotificationController, LogController],
+  controllers: [NotificationController, WebhookController, LogController],
   providers: [
     {
       provide: 'INotificationRepository',
       useClass: NotificationRepository,
+    },
+    {
+      provide: 'IWebhookRepository',
+      useClass: WebhookRepository,
     },
     NotificationFactory,
     EmailNotificationStrategy,
