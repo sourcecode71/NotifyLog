6c172a1867583315ea5c753f438c8bc32d89b655
Mostafiz <mostafiz571@gmail.com>
2025-05-05 15:30:13 +0600

    remove unnessary files

--- src/notification/domain/repositories/common/media-validation.repository.ts
+++
@@ -1,6 +0,0 @@
-import { Notification } from '../../entities/notification.entity';
-
-export interface IMediaValidationRepository {
-  save(validation: Notification): Promise<void>;
-  findUserByMedia(media: string): Promise<Notification | null>;
-}
